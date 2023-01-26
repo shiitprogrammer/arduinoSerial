ed5b2026251a641711170ea1c2e4f8dfcc37589d
shitprogrammer <82523549+shiitprogrammer@users.noreply.github.com>
2023-01-26 23:19:49 +0100

    Update sketch_jan23a.ino

--- sketch_jan23a/sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a/sketch_jan23a.ino
@@ -1,3 +1,32 @@
+int motor = 3;
+const int BUFFER_SIZE = 50;
+char buf[BUFFER_SIZE];
+
+void speedcontrol(int rpm) {
+      if( rpm >= 500 && rpm <= 4000)
+      { 
+        
+        int motorspeed = analogRead(A0);
+        int speed = int(map(rpm, 0,4000,0,255));
+        Serial.println(speed);
+        for (int i = 50;i<speed;i++){
+          analogWrite(motor, i);
+        
+        }
+      }
+      if(rpm == 0)
+      {
+        for (int i = 100 ;i>50;i--){
+        analogWrite(motor, i);
+        Serial.println("Slowing down");
+          if (i == 50){
+          digitalWrite(motor,0);
+          }
+        }
+      
+      }
+}
+
 void setup() {
   // put your setup code here, to run once:
 pinMode(3, OUTPUT);
@@ -8,33 +37,30 @@ int speed = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
-int motor = 3;
+String message = "";
+
 if ( Serial.available())
   {
-  int rpm = Serial.readString().toInt();
-   
-  if( rpm >= 500 && rpm <= 4000)
-    { 
-      
-      int motorspeed = analogRead(A0);
-      int speed = int(map(rpm, 0,4000,0,255));
-      Serial.println(speed);
-      for (int i = 50;i<speed;i++){
-        analogWrite(motor, i);
-      
-      }
-    }
-    if(rpm == 0)
-    {
-      for (int i = 100 ;i>50;i--){
-      analogWrite(motor, i);
-      Serial.println("Slowing down");
-        if (i == 50){
-        digitalWrite(motor,0);
-        }
-      }
+  int data = Serial.readBytes(buf, BUFFER_SIZE);
+  for(int i = 0; i < data; i++){
     
-    }
+    message += (buf[i]);
+    
   }
-  
+
+} 
+if (message == "conversation"){
+  Serial.println(analogRead(A0));
 }
+else if (message == "setrpm"){
+  Serial.println("Received");
+}
+  
+else if (message.toInt() >= 0) {
+  int rpm = message.toInt();
+  speedcontrol(rpm);
+}
+}
+
+
+
