cb5e467f7f63db61ef54064cf67ef08b5b6c4e69
shitprogrammer <82523549+shiitprogrammer@users.noreply.github.com>
2023-01-29 20:18:16 +0100

    cleaning up

--- sketch_jan23a/sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a/sketch_jan23a.ino
@@ -1,7 +1,7 @@
 int motor = 3;
 const int BUFFER_SIZE = 50;
 char buf[BUFFER_SIZE];
-
+int a = 50;
 void speedcontrol(int rpm) {
       if( rpm >= 500 && rpm <= 4000)
       { 
@@ -9,17 +9,17 @@ void speedcontrol(int rpm) {
         int motorspeed = analogRead(A0);
         int speed = int(map(rpm, 0,4000,0,255));
         Serial.println(speed);
-        for (int i = 50;i<speed;i++){
-          analogWrite(motor, i);
+        for (a; a<speed; a++){
+          analogWrite(motor, a);
         
         }
       }
       if(rpm == 0)
       {
-        for (int i = 100 ;i>50;i--){
-        analogWrite(motor, i);
-        Serial.println("Slowing down");
-          if (i == 50){
+        int b = a;
+        for (b ;b>=50;--b){
+        analogWrite(motor, b);
+          if (b == 50){
           digitalWrite(motor,0);
           }
         }
@@ -37,24 +37,19 @@ int speed = 0;
 
 void loop() {
   // put your main code here, to run repeatedly:
+char txtmessage[] = {};
 String message = "";
-
 if ( Serial.available())
   {
-  int data = Serial.readBytes(buf, BUFFER_SIZE);
-  for(int i = 0; i < data; i++){
-    
-    message += (buf[i]);
-    
+  message = Serial.readString();
+  message.toCharArray(txtmessage, BUFFER_SIZE);
+  for (int i=0;i<4;i++) {
+    Serial.print(txtmessage[i]);
   }
+  Serial.print(txtmessage[5]);
+  
 
 } 
-if (message == "conversation"){
-  Serial.println(analogRead(A0));
-}
-else if (message == "setrpm"){
-  Serial.println("Received");
-}
   
 else if (message.toInt() >= 0) {
   int rpm = message.toInt();
