bfe2ad2c0a278345a6f3391f4359643403f13cbb
shitprogrammer <82523549+shiitprogrammer@users.noreply.github.com>
2023-02-12 20:33:01 +0100

    update

--- sketch_jan23a/sketch_jan23a/sketch_jan23a.ino
+++ sketch_jan23a/sketch_jan23a/sketch_jan23a.ino
@@ -1,61 +1,49 @@
-int motor = 3;
-const int BUFFER_SIZE = 50;
-char buf[BUFFER_SIZE];
-int a = 50;
-void speedcontrol(int rpm) {
-      if( rpm >= 500 && rpm <= 4000)
-      { 
-        
-        int motorspeed = analogRead(A0);
-        int speed = int(map(rpm, 0,4000,0,255));
-        Serial.println(speed);
-        for (a; a<speed; a++){
-          analogWrite(motor, a);
-        
-        }
-      }
-      if(rpm == 0)
-      {
-        int b = a;
-        for (b ;b>=50;--b){
-        analogWrite(motor, b);
-          if (b == 50){
-          digitalWrite(motor,0);
-          }
-        }
-      
-      }
+int new_rpm = 0;
+int old_rpm;
+
+
+void blink() {
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(20);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(20);
 }
 
+void motorspeed(int new_speed,int old_speed, int motor) {
+  if (old_speed < new_speed)
+  {
+    for (int i= old_rpm; i< new_speed; i++) {
+      analogWrite(motor,i);
+      analogWrite(motor-2,i);
+      delay(10);
+      blink();
+    }
+  } 
+}
 void setup() {
   // put your setup code here, to run once:
 pinMode(3, OUTPUT);
-pinMode(5, OUTPUT);
-Serial.begin(115200);
-int speed = 0;
+pinMode(LED_BUILTIN, OUTPUT);
+Serial.begin(9600);
+old_rpm = 0;
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-char txtmessage[] = {};
 String message = "";
 if ( Serial.available())
   {
   message = Serial.readString();
-  message.toCharArray(txtmessage, BUFFER_SIZE);
-  for (int i=0;i<4;i++) {
-    Serial.print(txtmessage[i]);
+  int message_int = message.toInt();
+  new_rpm = map(message_int,0,4000,0,255);
+  motorspeed(new_rpm,old_rpm, 5);
+  
   }
-  Serial.print(txtmessage[5]);
-  
 
+old_rpm = new_rpm;
 } 
-  
-else if (message.toInt() >= 0) {
-  int rpm = message.toInt();
-  speedcontrol(rpm);
-}
-}
+
 
 
 
